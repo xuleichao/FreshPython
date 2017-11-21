9277e99b00323f6193eb23810dbf116c8f64d4a4
xuleichao <1205448156@qq.com>
2017-11-21 19:22:04 +0800

    toexcel

--- split_shiti.py
+++ split_shiti.py
@@ -5,16 +5,20 @@ by xlc time:2017-11-17 17:03:45
 import sys
 sys.path.append('D:/mypyfunc')
 import json
+import pandas
+from pandas import DataFrame
 
-dct = {'选文题目': '', '创建时间': '', '收藏次数': '', '上传用户': '', '题目': '', \
+
+if __name__ == '__main__':
+    f = open(r"D:\github\FreshPython\result.txt", 'r', encoding='utf-8')
+    lenth = set()
+    count = 0
+    result = []
+    for line in  f:
+        dct = {'选文题目': '', '创建时间': '', '收藏次数': '', '上传用户': '', '题目': '', \
        '题目编号': '', '浏览次数': '', '上传时间': '', '考点详细': '', \
        '命题类型': '', '质量平均': '', '是否推荐': '', '难度平均': '', \
        '所考词语': '', '使用次数': ''}
-
-if __name__ == '__main__':
-    f = open(r"D:\github\FreshPython\result6.txt", 'r', encoding='utf-8')
-    lenth = set()
-    for line in  f:
         lst = json.loads(line)
         tm = lst[0] # 题目
         #print(tm)
@@ -23,6 +27,31 @@ if __name__ == '__main__':
         for i in range(len(sx)):
             key = sx[i].split('：')
             new_sx[key[0]] = key[1]
-        lenth = lenth | set(new_sx.keys())
+        dct['选文题目'] = tm[0]
+        dct.update(new_sx)
+        if lst[2] != []:
+            dct['试题内容'] = lst[2][0]
+        else:
+            dct['试题内容'] = ''
+        if lst[3] != []:
+            dct['答案'] = lst[3][0]
+        else:
+            dct['答案'] = ''
+        #print(dct)
+        count += 1
+        result.append(dct)
+    df = DataFrame(result) # 创建一个DataFrame
+    shape = df.shape[0]
+    f = open('toexcel.txt', 'w', encoding='utf-8')
+    for i in range(shape):
+        data = df.iloc[i]
+        excel_data = data.tolist()
+        file_data = '\t'.join(excel_data)
+        f.write(file_data + '\n')
+    f.close()
+        
+        
+    
+    
+        
             
-    print(lenth)
